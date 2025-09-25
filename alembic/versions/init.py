4b21f741cefed5e7e097549bb487424318bf513b
mloso <copper.boyq@gmail.com>
2025-09-25 14:07:01 +0300

    Add alembic init file

---
+++ alembic/versions/init.py
@@ -0,0 +1,19 @@
+"""init
+
+Revision ID: init
+Revises:
+Create Date: 2025-09-24 16:15:53.755269
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "init"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None: ...
+
+
+def downgrade() -> None: ...
